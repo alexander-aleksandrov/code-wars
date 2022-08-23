55cf367d8f2e04445063f3616d07b154eab1281c
Aleksandr Aleksandrov <aleksandrov.aleksander.a@gmail.com>
2022-08-23 15:01:27 +0300

    Add the biggest prime factor solution

---
+++ CodeWars/Primes.cs
@@ -0,0 +1,18 @@
+﻿public static class Primes
+{
+  public static int GreatestPrimeFactor(long number)
+  {
+    int divisor = 2;
+    while (number > 1)
+    {
+      if (0 == (number % divisor))
+      {
+        number /= divisor;
+        divisor--;
+      }
+      divisor++;
+    }
+    return divisor;
+  }
+}
+
